06347b47e418f663c7a04ee2bb2a825f95879400
kenny the tomato <jcrossguitar@gmail.com>
2019-07-27 02:58:33 -0700

    rough version of the rpg

---
+++ assets/game.js
@@ -0,0 +1,16 @@
+var people = []
+var player;
+var computer;
+
+
+function Player(health, attack) {
+    this.health;
+    this.attack;
+}
+
+var playerAttack = function() {
+    var baseDamage;
+    if {
+
+    }
+}
